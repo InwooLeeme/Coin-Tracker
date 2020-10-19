e9776fecd5614a94872b7f88fe13606710d9cff8
ItsmeLee <arsmagica90@gmail.com>
2020-10-19 21:01:26 +0900

    Add new function
    Add the function to show the hourly variation of the coin and rank.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const API_URL = "https://api.coinpaprika.com/v1/tickers";
-const displayStatu = document.querySelector('.coinStatus');
+const nameInfo = document.querySelector('.nameInfo');
+const rankInfo = document.querySelector('.rankInfo');
+const priceInfo = document.querySelector('.price');
+const percentChange15m = document.querySelector('.percentChange15m');
+const percentChange1h = document.querySelector('.percentChange1h');
+const percentChange24h = document.querySelector('.percentChange24h');
+const requestingText = document.querySelector('h3');
 
 
 const getInfo = () => {
@@ -8,21 +14,41 @@ const getInfo = () => {
     })
     .then(response => response.json())
     .then(data => {
-        console.log(`Request`);
-        for (let index = 0; index < data.length; index++) {
-            let element = data[index];
-            let { name ,quotes:{USD} } = element;
-            let {price} = USD;
-            let li = document.createElement('li');
-            displayStatu.appendChild(li);
-            li.innerHTML = `${name} : ${price}`;
-        }
+        loadData(data)
     })
     .catch(error => console.log(error));
 }
 
-const init = () => {
-    setInterval(getInfo,5000);
+const loadData = (data) => {
+    for (let index = 0; index < data.length; index++) {
+        let element = data[index];
+        let { name,rank ,quotes:{USD} } = element;
+        let {price,percent_change_24h,percent_change_15m,percent_change_1h} = USD;
+        let nameList = document.createElement('li');
+        let rankList = document.createElement('li');
+        let priceList = document.createElement('li');
+        let minChangeList = document.createElement('li');
+        let hourChangeList = document.createElement('li');
+        let dayChangeList = document.createElement('li');
+        nameInfo.appendChild(nameList);
+        rankInfo.appendChild(rankList);
+        priceInfo.appendChild(priceList);
+        percentChange15m.appendChild(minChangeList);
+        percentChange1h.appendChild(hourChangeList);
+        percentChange24h.appendChild(dayChangeList);
+        nameList.innerHTML = `${name}`;
+        rankList.innerHTML = `${rank}`;
+        priceList.innerHTML = `${price}`;
+        minChangeList.innerHTML = `${percent_change_15m}`;
+        hourChangeList.innerHTML = `${percent_change_1h}`;
+        dayChangeList.innerHTML = `${percent_change_24h}`;
+    }
+    requestingText.classList.toggle('ESq77a');
+}
+
+const init = async () => {
+    setInterval(await getInfo,5000);
+    //await getInfo();
 }
 
 init();
