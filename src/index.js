c9ccfbad535b424adabd597663cdc69f6835c722
ItsmeLee <arsmagica90@gmail.com>
2020-10-19 12:16:47 +0900

    Get Coin infomation from api url
    Get some Information from api url.
    Then to console.log the information.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import "./styles.css";
-
 const API_URL = "https://api.coinpaprika.com/v1/tickers";
+
+const getInfo = () => {
+    const response = fetch(API_URL,{
+        method : 'GET'
+    })
+    .then(response => response.json())
+    .then(data => {
+        //let { name } = data;
+        let i = 0;
+        for (let index = 0; index < data.length; index++) {
+            let element = data[index];
+            
+            console.log(element.name);
+        }
+    })
+    .catch(error => console.log(error));
+}
+
+// price : data[].quotes.USD.price;
+
+const init = () => {
+    //setInterval(getInfo,5000);
+    getInfo();
+}
+
+init();
